609e23561d77ed46a0c288b5490cad16b4f8c086
Caio <caio.florees2005@gmail.com>
2023-12-21 11:19:49 -0300

    OrderStatus enum

--- src/main/java/com/caioprogramador/springbootproject/entities/Order.java
+++ src/main/java/com/caioprogramador/springbootproject/entities/Order.java
@@ -1,5 +1,6 @@
 package com.caioprogramador.springbootproject.entities;
 
+import com.caioprogramador.springbootproject.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -13,20 +14,41 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "tb_orders")
-@AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class Order implements Serializable {
+    @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Getter
+    @Setter
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+    @Setter
+    @Getter
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
+    }
 
     @Override
     public boolean equals(Object o) {
